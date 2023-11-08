accc523b493e49970a8520ac53a975217a8bfaf0
NikitaKhom <homutovnikita1@gmail.com>
2023-11-08 20:55:25 +0200

    the beginning

---
+++ лр4.1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double m, n, x, s;
+    cout << "Введіть загальну кількість людей: ";
+    cin >> m;
+    cout << "Введіть кількість людей в шерензі: ";
+    cin >> n;
+    x = int(m) / int(n);
+    s = int(m) % int(n);
+    cout << "s = " << s << endl;
+    cout << "x = " << x << endl;
+    system("pause");
+    return 0;
+}
